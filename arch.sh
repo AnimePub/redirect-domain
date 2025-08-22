cbdda435d8b9f0ae6d71eba7b7b199771c89dd9a
adnan123456a <abdullahaladnan95@gmail.com>
2025-08-22 12:19:16 -0400

    bash installation file uploaded

---
+++ arch.sh
@@ -0,0 +1,6 @@
+sudo pacman -S install npm
+sudo pacman -S install git
+git clone https://github.com/AnimePub/AniPub.git
+cd AniPun
+npm install
+npm run start
---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt install npm
+sudo apt install git
+git clone https://github.com/AnimePub/AniPub.git
+cd AniPun
+npm install
+npm run start
