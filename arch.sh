bc1105d1e1350aab25b68a49a37cb6ead07a8336
adnan123456a <abdullahaladnan95@gmail.com>
2025-08-22 12:22:49 -0400

    bash init

--- arch.sh
+++ arch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo pacman -S install npm
 sudo pacman -S install git
 git clone https://github.com/AnimePub/AniPub.git
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt install npm
 sudo apt install git
 git clone https://github.com/AnimePub/AniPub.git
