47ef6a4446992ea1addea97df563d02a93037b99
Abdullah Al Adnan <130243149+adnan123456a@users.noreply.github.com>
2025-09-05 09:34:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 sudo apt install npm
 sudo apt install git
-git clone https://github.com/AnimePub/AniPub.git
-cd AniPun
-npm install
+if [ -v ~/AniPub ]
+then 
+    cd ~/AniPun
+    npm install
+else 
+    echo "Couldn't Find AniPub .."
+    echo "Downloading It"
+    git clone https://github.com/AnimePub/AniPub.git
+    cd ~/AniPun
+    npm install
+fi
 npm run start
